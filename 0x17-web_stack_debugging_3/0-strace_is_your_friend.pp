f86846ad4b6fdb2b881b2c4065f7a63aafc071d7
lepc1972 <githublepc@gmail.com>
2021-09-28 13:01:07 -0500

    puppet n readme

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# wordpress apache2 running again
+exec { 'fixing-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
